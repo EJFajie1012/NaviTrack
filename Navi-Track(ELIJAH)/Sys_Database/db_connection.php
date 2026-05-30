e96eb2f1d88a6766080944ecd3829b072957c4e5
EJFajie1012 <ejfajardo067@gmail.com>
2026-05-30 23:37:48 +0800

    Delete Navi-Track(ELIJAH) directory

--- Navi-Track(ELIJAH)/Sys_Database/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbserver = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "campusnav";
-
-$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
-?>
--- Navi-Track(ELIJAH)/Sys_Login/hash.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo password_hash("student01", PASSWORD_DEFAULT);
-?>
